b4c3d0f7ff9c7b91dacf3424e933f2298ccdc198
Md Touhidul Islam <md.touhidul.islam.96@gmail.com>
2022-08-29 21:12:39 +0600

    Create 2 - Java Stdin and Stdout I.java

---
+++ 2 - Java Stdin and Stdout I.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        
+        int int1 = scanner.nextInt();
+        int int2 = scanner.nextInt();
+        int int3 = scanner.nextInt();
+        
+        System.out.println(int1 + "\n" + int2 + "\n" + int3);
+    }
+}
