552335ff34ac62772a9bb3cf7490af3747bb6fc3
Md Touhidul Islam <md.touhidul.islam.96@gmail.com>
2022-08-30 21:52:56 +0600

    Create Java Loops II.java

---
+++ Java Loops II.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        int q = scanner.nextInt(); //how many times we wanna solve the problem/how many queries
+
+        int a, b, n = 0; //declaring variables for query values
+
+        while (q != 0) { //scanning variables..
+            int s = 0, temp = 0; //for storing result
+
+            a = scanner.nextInt();
+            b = scanner.nextInt();
+            n = scanner.nextInt();
+
+            for (int i = 0; i <= n - 1; i++) {
+                temp = temp + (int) (Math.pow(2, i) * b);
+                s = a + temp; //cz a is constant and only b is changing so b in temp result and a in final result
+
+                System.out.print(s + " ");
+            }
+            System.out.println(); //new line 2nd query
+
+            q--; //while loop count
+        }
+    }
+}
