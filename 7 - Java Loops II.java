f0155c9807befe87aaab2adc48696fb6a1ac1de2
Md Touhidul Islam <md.touhidul.islam.96@gmail.com>
2022-08-30 21:57:13 +0600

    Update 7 - Java Loops II.java

--- 7 - Java Loops II.java
+++ 7 - Java Loops II.java
@@ -9,14 +9,14 @@ public class Solution {
 
         int a, b, n = 0; //declaring variables for query values
 
-        while (q != 0) { //scanning variables..
+        while (q != 0) { //loop for each query
             int s = 0, temp = 0; //for storing result
 
             a = scanner.nextInt();
             b = scanner.nextInt();
             n = scanner.nextInt();
 
-            for (int i = 0; i <= n - 1; i++) {
+            for (int i = 0; i <= n - 1; i++) { //loop for n
                 temp = temp + (int) (Math.pow(2, i) * b);
                 s = a + temp; //cz a is constant and only b is changing so b in temp result and a in final result
 
